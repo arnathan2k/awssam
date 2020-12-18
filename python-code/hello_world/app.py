b89dcd3451db2682eb0b490085045af59c748316
arnathan2k <arnathan2k@gmail.com>
2020-12-18 15:20:59 -0700

    small sysout changes

--- python-code/hello_world/app.py
+++ python-code/hello_world/app.py
@@ -1,30 +1,25 @@
 import json
-
+import os
+import boto3
+from botocore.exceptions import ClientError
 # import requests
 
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
-
     Parameters
     ----------
     event: dict, required
         API Gateway Lambda Proxy Input Format
-
         Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
     context: object, required
         Lambda Context runtime methods and attributes
-
         Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
     Returns
     ------
     API Gateway Lambda Proxy Output Format: dict
-
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
-
     # try:
     #     ip = requests.get("http://checkip.amazonaws.com/")
     # except requests.RequestException as e:
@@ -33,10 +28,25 @@ def lambda_handler(event, context):
 
     #     raise e
 
+    #session = boto3.Session(aws_access_key_id='read from ENV',aws_secret_access_key='REad from Env')
+    #config = session.client('config',region_name='us-east-1')
+
+
+    config = boto3.client('config')
+    try:
+        response = config.describe_config_rules()
+        fails = config.__module__.
+        for rule in response['ConfigRules']:
+            print("***********************************************")            
+            print(rule,'   \n')
+    except ClientError as e:
+        print(e)
+
+
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "hello world *******************",
             # "location": ip.text.replace("\n", "")
         }),
     }
